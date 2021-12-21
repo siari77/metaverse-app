334a537d73f363eea601a936cf438fb8077ad66d
George Siaravas <siaravas_giorgos@hotmail.com>
2021-12-21 13:57:47 +0200

    chat func complete

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { useMoralis} from "react-moralis";
+import Avatar from "../components/Avatar";
+import ChangeUsername from "../components/ChangeUsername";
+
+function Header() {
+    const {user} = useMoralis();
+    return (
+        <div className="sticky top-0 p-5 z-50 bg-black shadow-sm text-pink-500 border-b-2 border-pink-700">
+            <div className="grid grid-cols-5 lg:grid-cols-6 items-end lg:items-center">
+                <div className="relative h-24 w-24 mx-auto hiiden lg:inline-grid">
+                    <Image layout="fill" className="rounded-full object-cover" src="https://links.papareact.com/3pi"/>
+                </div>
+
+                <div className="col-span-4 text-left lg:text-center">
+                    <div className="relative h-48 w-48 lg:mx-auto border-pink-500 border-8 rounded-full">
+                        <Avatar logoutOnPress/>
+                    </div>
+
+                    <h1 className="text-3xl">Metaverse Chat</h1>
+
+                    <h2 className="text-5xl font-bold truncate">{user.getUsername()}</h2>
+
+                    <ChangeUsername />
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
